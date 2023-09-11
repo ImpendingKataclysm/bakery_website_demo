bcbc66957db3cbbb93ca28dd805a160d1910729e
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-11 15:18:47 -0700

    Create a contact message model form based on the contact message model

--- main_site/forms.py
+++ main_site/forms.py
@@ -52,3 +52,47 @@ class ApplicantForm(forms.ModelForm):
             'date_available',
             'resume',
         )
+
+
+class ContactForm(forms.ModelForm):
+    """
+    Defines the contact page form through which visitors may send the site
+    admin messages. Contains the following fields:
+    - name: sender name
+    - email: sender email address
+    - message: message content
+    Data send via the contact form is stored in the Contact Messages table in
+    the database.
+    """
+    name = forms.CharField(
+        max_length=models.CHRFIELD_LEN,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your name',
+        })
+    )
+
+    email = forms.EmailField(
+        max_length=320,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email',
+        })
+    )
+
+    message = forms.CharField(
+        max_length=models.TXTFIELD_LEN,
+        required=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Message',
+            'rows': 6,
+        })
+    )
+
+    class Meta:
+        model = models.ContactMessage
+        fields = ('name', 'email', 'message')
+
