01bba2f5c7787f0bf095e1b329a365ae6d91e0f4
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-11 14:31:29 -0700

    Create ApplicationForm based on the Applicant model and display the form on the Careers page. When the form is submitted and validated, the applicant is saved in the database.

--- main_site/views.py
+++ main_site/views.py
@@ -33,18 +33,13 @@ class ProductPage(generic.ListView):
 
 
 class JobApplicationPage(generic.FormView):
+    """
+    Display job application form on the Careers page. When the form is submitted,
+    save the applicant to the database
+    """
     form_class = forms.ApplicantForm
     template_name = 'main/jobs.html'
     success_url = '/'
-
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.render_to_response(self.get_context_data(form=form))
 
     def form_valid(self, form):
         success_message = 'Thank you for your application!'
