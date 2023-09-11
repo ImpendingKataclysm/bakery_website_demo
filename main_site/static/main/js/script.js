7678d151d1350eaa2cf123a44fedd7a0cb444708
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-11 00:24:27 -0700

    Display products from the database on the products page with responsive styles and a collapsible page nav

--- main_site/static/main/js/script.js
+++ main_site/static/main/js/script.js
@@ -6,3 +6,21 @@ const nav_btn = document.getElementById('nav-btn');
 nav_btn.addEventListener('click', () => {
     nav_bar.classList.toggle('show');
 });
+
+/* Toggle Side Nav Visibility */
+
+const side_nav = document.getElementById('side_nav');
+const side_nav_btn = document.getElementById('side_nav_btn');
+const side_nav_items = document.getElementsByClassName('sidenav-item');
+
+if(side_nav && side_nav_btn && side_nav_items) {
+    side_nav_btn.addEventListener('click', () => {
+        side_nav.classList.toggle('show');
+    });
+
+    for (let i = 0; i < side_nav_items.length; i++) {
+        side_nav_items[i].addEventListener('click', () => {
+            side_nav.classList.toggle('show');
+        });
+    }
+}
--- main_site/views.py
+++ main_site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from . import models
 
 
 class HomePage(generic.TemplateView):
@@ -10,11 +11,24 @@ class HomePage(generic.TemplateView):
 
 
 class AboutPage(generic.TemplateView):
+    """
+    Displays the about page.
+    """
     template_name = 'main/about.html'
 
 
-class ProductPage(generic.TemplateView):
+class ProductPage(generic.ListView):
+    """
+    Display the products in the database on the products page, organized by
+    category.
+    """
     template_name = 'main/products.html'
+    queryset = models.Product.objects.order_by('-date_added')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['types'] = models.PRODUCT_TYPE
+        return context
 
 
 class JobApplicationPage(generic.TemplateView):
